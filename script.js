d8c419e5740c33d74f59443a39864ae357879058
Mrinmoy Deb <mrinmoydeb485@gmail.com>
2023-02-08 01:10:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// SEARCH BOX SLIDE START
+
+let searchForm = document.querySelector(".search-form")
+   document.querySelector("#srch-btn").onclick = ()=>{
+   searchForm.classList.toggle('active');
+   }
+// SEARCH BOX SLIDE END
+
+// CART BOX START
+let cartBtn = document.querySelector(".shopping-cart")
+   document.querySelector("#cart-btn").onclick = ()=>{
+   cartBtn.classList.toggle('active');
+   }
+// CART BOX START
